8e40ee1169f0743d401a04f5d4863a2fbb082bd2
serifekilic <purdemirderya@yahoo.com>
2022-09-22 13:46:25 -0400

    Initial commit

---
+++ src/main/java/com/aktser/AktSerSchoolApplication.java
@@ -0,0 +1,13 @@
+package com.aktser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AktSerSchoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AktSerSchoolApplication.class, args);
+	}
+
+}
