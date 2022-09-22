351ff9edcd07993d268abf0daec35096efda827d
serifekilic <purdemirderya@yahoo.com>
2022-09-22 14:41:01 -0400

    created controller and module packages

--- src/main/java/com/aktser/AktSerSchoolApplication.java
+++ src/main/java/com/aktser/AktSerSchoolApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AktSerSchoolApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AktSerSchoolApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AktSerSchoolApplication.class, args);
+    }
 
 }
